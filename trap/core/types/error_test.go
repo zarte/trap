bf83e073152e2a2fab5c54de7fd55f63e69a2b37
rain <raincious@gmail.com>
2016-02-24 22:09:21 +0800

    - Add test cases for `Error` and `Throw` - Add test cases for `Mutex`

---
+++ trap/core/types/error_test.go
@@ -0,0 +1,105 @@
+/*
+ * Trap
+ * An anti-pryer server for better privacy
+ *
+ * This file is a part of Trap project
+ *
+ * Copyright 2016 Rain Lee <raincious@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package types
+
+import (
+    "testing"
+    "errors"
+    "fmt"
+)
+
+func TestErrorThrow(t *testing.T) {
+    err := NewError("This is a test error: %s %s %s")
+    throw := err.Throw("some", "error", "information")
+
+    if throw.parentRef != err.ref ||
+    throw.parentErr != err.error ||
+    throw.message != fmt.Sprintf(err.text, "some", "error", "information") {
+        t.Error("Error can't build correct Throw info")
+    }
+}
+
+func TestThrowIs(t *testing.T) {
+    err1 := NewError("Test error 1")
+    err2 := NewError("Test error 2")
+
+    if !err1.Throw().Is(err1) || err1.Throw().Is(err2) {
+        t.Error("Throw failed compare it's parent error")
+    }
+}
+
+func TestThrowIsError(t *testing.T) {
+    err1 := errors.New("Test error 1")
+    err2 := errors.New("Test error 2")
+
+    thr1 := ConvertError(err1)
+
+    if !thr1.IsError(err1) || thr1.IsError(err2) {
+        t.Error("Throw failed compare it's parent error")
+    }
+}
+
+func TestThrowError(t *testing.T) {
+    throw   :=  NewError("Test error 1").Throw()
+
+    if throw.Error() != "Test error 1" {
+        t.Error("Error message is incorrect")
+    }
+
+    throw   =   NewError("Test error 1 %s %s %s").
+                    Throw("formated", "strings", "here")
+
+    if throw.Error() != "Test error 1 formated strings here" {
+        t.Error("Error message is incorrect")
+    }
+}
+
+func TestThrowMarshalJSON(t *testing.T) {
+    json, jsonErr   :=  NewError("Test error 1").Throw().MarshalJSON()
+
+    if jsonErr != nil {
+        t.Error("Failed marshal Throw information to JSON")
+
+        return
+    }
+
+    if string(json) != "\"Test error 1\"" {
+        t.Error("Throw generated an invalid JSON result")
+
+        return
+    }
+
+    json, jsonErr   =   NewError("Test error 1 \"%s\" \"%s\" \"%s\"").
+                            Throw("formated", "strings", "here").MarshalJSON()
+
+    if jsonErr != nil {
+        t.Error("Failed marshal Throw information to JSON")
+
+        return
+    }
+
+    if string(json) != "\"Test error 1 \\\"formated\\\" \\\"strings\\\" \\\"here\\\"\"" {
+        t.Error("Throw generated an invalid JSON result")
+
+        return
+    }
+}
---
+++ trap/core/types/mutex_test.go
@@ -0,0 +1,114 @@
+/*
+ * Trap
+ * An anti-pryer server for better privacy
+ *
+ * This file is a part of Trap project
+ *
+ * Copyright 2016 Rain Lee <raincious@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package types
+
+import (
+    "testing"
+    "time"
+    "sync"
+)
+
+func testMutexExec(t *testing.T) {
+    mx      := Mutex{}
+    tWG1    := sync.WaitGroup{}
+    tWG2    := sync.WaitGroup{}
+    val     := []byte{}
+
+    tWG1.Add(1)
+    tWG2.Add(1)
+
+    go mx.Exec(func() { // Callback 1
+        defer tWG2.Done()
+
+        tWG1.Done()
+
+        time.Sleep(1 * time.Second)
+
+        val = append(val, 'A')
+    })
+
+    tWG1.Wait() // We can confirm the callback 1 is running
+
+    tWG2.Add(1)
+
+    go mx.Exec(func() { // Callback 2
+        defer tWG2.Done()
+
+        val = append(val, 'B')
+    })
+
+    tWG2.Wait()
+
+    if string(val) != "AB" {
+        t.Error("Mutex failed to control the running order")
+    }
+}
+
+func TestMutexExec(t *testing.T) {
+    // Test 3 times to avoid any false negative
+    testMutexExec(t)
+    testMutexExec(t)
+    testMutexExec(t)
+}
+
+func testMutexRoutineExec(t *testing.T) {
+    mx      := Mutex{}
+    tWG1    := sync.WaitGroup{}
+    tWG2    := sync.WaitGroup{}
+    val     := []byte{}
+
+    tWG1.Add(1)
+    tWG2.Add(1)
+
+    mx.RoutineExec(func() { // Callback 1
+        defer tWG2.Done()
+
+        tWG1.Done()
+
+        time.Sleep(1 * time.Second)
+
+        val = append(val, 'A')
+    })
+
+    tWG1.Wait() // We can confirm the callback 1 is running
+
+    tWG2.Add(1)
+
+    mx.RoutineExec(func() { // Callback 2
+        defer tWG2.Done()
+
+        val = append(val, 'B')
+    })
+
+    tWG2.Wait()
+
+    if string(val) != "AB" {
+        t.Error("RoutineExec failed to control the running order")
+    }
+}
+
+func TestMutexRoutineExec(t *testing.T) {
+    // Test 3 times to avoid any false negative
+    testMutexRoutineExec(t)
+    testMutexRoutineExec(t)
+    testMutexRoutineExec(t)
+}
